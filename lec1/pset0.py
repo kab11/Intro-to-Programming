45cebfb1a36c244d46a6cb4024a63227a6dc54bf
Karynne Black <kblack@kblacks-MacBook-Pro.local>
2020-05-05 14:27:47 -0700

    Upload pset0 program

---
+++ lec1/pset0.py
@@ -0,0 +1,11 @@
+# Problem Set 0
+# Name: kblack
+# Time: 0:15
+
+last_name = input('Please enter your last name: ')
+
+first_name = input('Please enter your first name: ')
+
+print(first_name)
+print(last_name)
+
